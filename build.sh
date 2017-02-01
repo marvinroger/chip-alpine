1603fa3d7e9e5f31808c80896624a7e4104a6bd8
Marvin Roger <nivramdu94@gmail.com>
2017-02-01 12:21:33 +0100

    :art: :racehorse: Remove the need for Docker

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 set -eo pipefail
 
-readonly ALPINE_VERSION="edge"
-readonly LATEST_BASEBUILD_URL="http://opensource.nextthing.co/chip/buildroot/stable/latest"
+readonly ALPINE_CHROOT_INSTALL_VERSION="0.1.1"
+readonly ALPINE_VERSION="latest-stable"
+readonly LATEST_BUILDROOT_URL="http://opensource.nextthing.co/chip/buildroot/stable/latest"
 
 readonly GITHUB_REPO="marvinroger/chip-alpine"
 readonly GITHUB_LOGIN_USERNAME="marvinroger"
@@ -11,159 +12,248 @@ readonly GITHUB_LOGIN_USERNAME="marvinroger"
 
 CWD=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 WORKING_DIR=$(mktemp -d -p /tmp chip-alpine.XXXXXX)
-mkdir -p "${WORKING_DIR}/basebuild/extracted"
-BASEBUILD_DIR="${WORKING_DIR}/basebuild"
-mkdir -p "${WORKING_DIR}/alpine"
+BUILDROOT_DIR="${WORKING_DIR}/buildroot"
+mkdir -p "${BASEBUILD_DIR}"
 ALPINE_DIR="${WORKING_DIR}/alpine"
-mkdir -p "${WORKING_DIR}/alpine-build/images"
-ALPINE_BUILD_DIR="${WORKING_DIR}/alpine-build"
+mkdir -p "${ALPINE_DIR}"
+CHIP_BUILD_DIR="${WORKING_DIR}/chip-build"
+mkdir -p "${CHIP_BUILD_DIR}/images"
+
+die () {
+	printf '\033[1;31mERROR:\033[0m %s\n' "$@" >&2  # bold red
+	exit 1
+}
+
+einfo () {
+	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2  # bold cyan
+}
+
+ewarn () {
+	printf '\033[1;33m> %s\033[0m\n' "$@" >&2  # bold yellow
+}
+
+install_apt_dependencies () {
+  apt-get install -y git liblzo2-dev python-lzo mtd-utils
+}
+
+install_ubi_reader () {
+  local temp_dir=$(mktemp -d -p /tmp ubi_reader.XXXXXX)
+  git clone https://github.com/jrspruitt/ubi_reader "${temp_dir}"
+  pushd "${temp_dir}"
+  python setup.py install
+  popd
+  rm --recursive "${temp_dir}"
+}
+
+install_alpine_chroot_install () {
+  local version="${1}"
+
+  wget --quiet --output-document /usr/local/bin/alpine-chroot-install "https://raw.githubusercontent.com/jirutka/alpine-chroot-install/v${version}/alpine-chroot-install"
+  chmod +x /usr/local/bin/alpine-chroot-install
+}
+
+get_latest_buildroot () {
+  local latest_buildroot_url="${1}"
+  local buildroot_dir="${2}"
+  
+  local latest_buildroot="$(wget -q -O- ${latest_buildroot_url})"
+  eval $3="${latest_buildroot}"
+  local buildroot_rootfs_url="${latest_buildroot}/images/rootfs.ubi"
+  
+  local temp_dir=$(mktemp -d -p /tmp buildroot.XXXXXX)
+  
+  # download buildroot
+  wget --quiet --output-document "${temp_dir}/rootfs.ubi" "${buildroot_rootfs_url}"
+  
+  # extract ubi
+  pushd "${temp_dir}"
+  mkdir extracted
+  pushd extracted
+  ubireader_extract_files "../rootfs.ubi"
+  pushd ubifs-root
+  pushd "$(find . -maxdepth 1 ! -path .|head -n 1)"
+  pushd rootfs
+  cp --archive ./. "${buildroot_dir}"
+  popd
+  popd
+  popd
+  popd
+  popd
+  rm --recursive "${temp_dir}"
+}
+
+prepare_alpine () {
+  local alpine_version="${1}"
+  local alpine_dir="${2}"
+
+  CHROOT_KEEP_VARS="" ALPINE_PACKAGES="wpa_supplicant wireless-tools bkeymaps tzdata nano" alpine-chroot-install -d "${alpine_dir}" -a armhf -b "${alpine_dir}" -m http://dl-cdn.alpinelinux.org/alpine/
+  
+  "${alpine_dir}/enter-chroot" root <<-EOF
+    set -e
+    # Needed services
+    rc-update add devfs sysinit
+    rc-update add dmesg sysinit
+    rc-update add mdev sysinit
+  
+    rc-update add modules boot
+    rc-update add sysctl boot
+    rc-update add hostname boot
+    rc-update add bootmisc boot
+    rc-update add syslog boot
+    rc-update add wpa_supplicant boot # needed, otherwise does not connect after reboot
+  
+    rc-update add mount-ro shutdown
+    rc-update add killprocs shutdown
+    rc-update add savecache shutdown
+  
+    # Allow root login with no password.
+    passwd root -d
+  
+    # Allow root login from serial.
+    echo ttyS0 >> /etc/securetty
+    echo ttyGS0 >> /etc/securetty
+  
+    # Make sure the USB virtual serial device is available.
+    echo g_serial >> /etc/modules
+  
+    # Make sure wireless networking is available.
+    echo 8723bs >> /etc/modules
+  
+    # These enable the USB virtual serial device, and the standard serial
+    # pins to both be used as TTYs
+    echo ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt102 >> /etc/inittab
+    echo ttyGS0::respawn:/sbin/getty -L ttyGS0 115200 vt102 >> /etc/inittab
+EOF
+}
+
+prepare_rootfs () {
+  local buildroot_dir="${1}"
+  local alpine_dir="${2}"
+  local ubi_dest="${3}"
+  
+  local temp_dir=$(mktemp -d -p /tmp ubi.XXXXXX)
+  
+  cp --archive "${buildroot_dir}/boot/." "${alpine_dir}/boot"
+  cp --archive "${buildroot_dir}/lib/modules/." "${alpine_dir}/lib/modules"
+  
+  pushd "${temp_dir}"
+  cat <<EOF >ubinize.cfg
+  [ubifs]
+  mode=ubi
+  vol_id=0
+  vol_type=dynamic
+  vol_name=rootfs
+  vol_alignment=1
+  vol_flags=autoresize
+  image=rootfs.ubifs
+EOF
+  mkfs.ubifs -d "${alpine_dir}" -o rootfs.ubifs -e 0x1f8000 -c 2000 -m 0x4000 -x lzo
+  ubinize -o "${ubi_dest}" -m 0x4000 -p 0x200000 -s 16384 ubinize.cfg
+  popd
+  rm --recursive "${temp_dir}"
+}
+
+make_alpine_release () {
+  local chip_build_dir="${1}"
+  local latest_buildroot="${2}"
+  local tar_dest="${3}"
+  
+  wget --quiet --output-document "${chip_build_dir}/images/sun5i-r8-chip.dtb" "${latest_buildroot}/images/sun5i-r8-chip.dtb"
+  wget --quiet --output-document "${chip_build_dir}/images/sunxi-spl.bin" "${latest_buildroot}/images/sunxi-spl.bin"
+  wget --quiet --output-document "${chip_build_dir}/images/sunxi-spl-with-ecc.bin" "${latest_buildroot}/images/sunxi-spl-with-ecc.bin"
+  wget --quiet --output-document "${chip_build_dir}/images/uboot-env.bin" "${latest_buildroot}/images/uboot-env.bin"
+  wget --quiet --output-document "${chip_build_dir}/images/zImage" "${latest_buildroot}/images/zImage"
+  wget --quiet --output-document "${chip_build_dir}/images/u-boot-dtb.bin" "${latest_buildroot}/images/u-boot-dtb.bin"
+  
+  tar -zcv -C "${chip_build_dir}" -f "${tar_dest}" "${chip_build_dir}"
+}
+
+gather_rootfs_versions () {
+  local buildroot_dir="${1}"
+  local alpine_dir="${2}"
+  
+  source "${buildroot_dir}/etc/os-release"
+  eval $3="${VERSION_ID}"
+  
+  source "${alpine_dir}/etc/os-release"
+  eval $4="${VERSION_ID}"
+  eval $5="${PRETTY_NAME}"
+}
+
+release_github () {
+  local repo="${1}"
+  local username="${2}"
+  local acess_token="${3}"
+  local tag_name="${4}"
+  local release_name="${5}"
+  local release_body="${6}"
+  local tar_location="${7}"
+  
+  local release_json=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "%s","draft": false,"prerelease": false}' "${tag_name}" "${release_name}" "${release_body}")
+  local github_release_id=$(curl -u "${username}:${access_token}" --data "${release_json}" -v --silent "https://api.github.com/repos/${repo}/releases" 2>&1 | sed -ne 's/^  "id": \(.*\),$/\1/p')
+  
+  curl -u "${username}:${access_token}" -X POST -H "Content-Type: application/gzip" --data-binary "@${tar_location}" "https://uploads.github.com/repos/${repo}/releases/${github_release_id}/assets?name=${tag_name}.tar.gz"
+}
 
 #####
 # Install dependencies
 #####
 
-echo "Installing dependencies..."
+einfo "Installing dependencies..."
+install_apt_dependencies
 
-# apk dependencies
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-apk update
-apk add ca-certificates wget curl build-base git python2 py2-pip python2-dev lzo-dev tar mtd-utils
+einfo "Installing ubi_reader..."
+install_ubi_reader
 
-update-ca-certificates
-
-# python-lzo
-pip install python-lzo
-
-# ubi_reader
-git clone https://github.com/jrspruitt/ubi_reader "${WORKING_DIR}/ubi_reader"
-cd "${WORKING_DIR}/ubi_reader"
-python setup.py install
+einfo "Installing alpine-chroot-install..."
+install_alpine_chroot_install "${ALPINE_CHROOT_INSTALL_VERSION}"
 
 #####
 # Get the latest base buildroot image
 #####
 
-echo "Getting latest base buildroot image..."
-
-LATEST_BASEBUILD="$(wget -q -O- ${LATEST_BASEBUILD_URL})" || (echo "ERROR: cannot reach ${LATEST_BASEBUILD_URL}" && exit 1)
-if [[ -z "${LATEST_BASEBUILD}" ]]; then
-  echo "error: could not get URL for latest build from ${LATEST_BASEBUILD_URL}"
-  exit 1
-fi
-
-BASEBUILD_ROOTFS_URL="${LATEST_BASEBUILD}/images"
-
-# download basebuild
-wget -q -P "${BASEBUILD_DIR}" "${BASEBUILD_ROOTFS_URL}/rootfs.ubi"
-
-#####
-# Extract the ubi file to get the kernel
-#####
-
-echo "Extracting buildroot image to get the kernel..."
-
-cd "$BASEBUILD_DIR/extracted"
-ubireader_extract_files ../rootfs.ubi
-cd ubifs-root
-cd "$(find . -maxdepth 1 ! -path .|head -n 1)"
-cd rootfs
-# shellcheck disable=SC1091
-source etc/os-release
-BUILDROOT_VERSION_ID=${VERSION_ID}
-cp -R boot "$BASEBUILD_DIR/extracted"
-cp -R lib/modules "$BASEBUILD_DIR/extracted"
+einfo "Getting latest buildroot..."
+local latest_buildroot=""
+get_latest_buildroot "${LATEST_BUILDROOT_URL}" "${BUILDROOT_DIR}" "latest_buildroot"
 
 #####
 # Get and set-up Alpine
 #####
 
-echo "Getting and setting-up Alpine..."
-
-cd "$ALPINE_DIR"
-mkdir rootfs
-apk -X "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/main" -U --allow-untrusted --root ./rootfs --initdb add alpine-base alpine-mirrors || true
-# Install packages needed for wireless networking + nano + tzdata and bkeymaps needed for setup-alpine
-apk -X "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/main" -U --allow-untrusted --root ./rootfs add wpa_supplicant wireless-tools bkeymaps tzdata nano || true
-
-# shellcheck disable=SC1091
-source rootfs/etc/os-release
-ALPINE_VERSION_ID=${VERSION_ID}
-ALPINE_PRETTY_NAME=${PRETTY_NAME}
-
-cp /usr/bin/qemu-arm-static rootfs/usr/bin/
-cp /etc/resolv.conf rootfs/etc/
-mount -t proc none rootfs/proc
-mount -o bind /sys rootfs/sys
-mount -o bind /dev rootfs/dev
-
-# Setup Alpine from the inside
-cp "${CWD}/chroot_build.sh" rootfs/usr/bin
-chmod +x rootfs/usr/bin/chroot_build.sh
-chroot rootfs /usr/bin/chroot_build.sh
-
-umount rootfs/proc
-umount rootfs/sys
-umount rootfs/dev
-
-rm rootfs/usr/bin/qemu-arm-static
-rm rootfs/etc/resolv.conf
-rm rootfs/usr/bin/chroot_build.sh
+einfo "Getting and setting-up Alpine..."
+prepare_alpine "${ALPINE_VERSION}" "${ALPINE_DIR}"
 
 #####
 # Prepare rootfs
 #####
 
-echo "Preparing rootfs..."
-
-cp -R "${BASEBUILD_DIR}/extracted/boot" rootfs/boot
-cp -R "${BASEBUILD_DIR}/extracted/modules" rootfs/lib/modules
-
-cat <<EOF >ubinize.cfg
-[ubifs]
-mode=ubi
-vol_id=0
-vol_type=dynamic
-vol_name=rootfs
-vol_alignment=1
-vol_flags=autoresize
-image=rootfs.ubifs
-EOF
-mkfs.ubifs -d rootfs -o rootfs.ubifs -e 0x1f8000 -c 2000 -m 0x4000 -x lzo
-ubinize -o rootfs.ubi -m 0x4000 -p 0x200000 -s 16384 ubinize.cfg
+einfo "Preparing rootfs..."
+prepare_rootfs "${BUILDROOT_DIR}" "${ALPINE_DIR}" "${CHIP_BUILD_DIR}/images/rootfs.ubi"
 
 #####
 # Make Alpine release
 #####
 
-echo "Making Alpine release..."
+einfo "Making Alpine release..."
+make_alpine_release "${CHIP_BUILD_DIR}" "${latest_buildroot}" "./alpine.tar.gz"
 
-cd "${ALPINE_BUILD_DIR}/images"
-cp "${ALPINE_DIR}/rootfs.ubi" ./
-
-wget -q "${BASEBUILD_ROOTFS_URL}/sun5i-r8-chip.dtb"
-wget -q "${BASEBUILD_ROOTFS_URL}/sunxi-spl.bin"
-wget -q "${BASEBUILD_ROOTFS_URL}/sunxi-spl-with-ecc.bin"
-wget -q "${BASEBUILD_ROOTFS_URL}/uboot-env.bin"
-wget -q "${BASEBUILD_ROOTFS_URL}/zImage"
-wget -q "${BASEBUILD_ROOTFS_URL}/u-boot-dtb.bin"
-
-tar -zcv -C "${WORKING_DIR}" -f "${WORKING_DIR}/alpine.tar.gz" alpine-build
+einfo "Gathering rootfs versions..."
+local buildroot_version_id=""
+local alpine_version_id=""
+local alpine_pretty_name=""
+gather_rootfs_versions "${BUILDROOT_DIR}" "${ALPINE_DIR}" "buildroot_version_id" "alpine_version_id" "alpine_pretty_name"
 
 #####
 # Create GitHub release
 #####
 
-echo "Releasing on GitHub..."
+einfo "Releasing on GitHub..."
+release_github "${GITHUB_REPO}" "${GITHUB_LOGIN_USERNAME}" "${GITHUB_ACCESS_TOKEN}" \
+  "alpine-${alpine_version_id}_buildroot-${buildroot_version_id}_$(date +%s)" \
+  "${alpine_pretty_name} with Buildroot ${buildroot_version_id} built on $(date +%Y-%m-%d)" \
+  "Daily build." \
+  "./alpine.tar.gz"
 
-TAG_NAME="alpine-${ALPINE_VERSION_ID}_buildroot-${BUILDROOT_VERSION_ID}_$(date +%s)"
-RELEASE_NAME="${ALPINE_PRETTY_NAME} with Buildroot ${BUILDROOT_VERSION_ID} built on $(date +%Y-%m-%d)"
-RELEASE_BODY="Nightly build."
-
-RELEASE_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "%s","draft": false,"prerelease": false}' "$TAG_NAME" "$RELEASE_NAME" "$RELEASE_BODY")
-GITHUB_RELEASE_ID=$(curl -u "${GITHUB_LOGIN_USERNAME}:${GITHUB_ACCESS_TOKEN}" --data "$RELEASE_JSON" -v --silent "https://api.github.com/repos/${GITHUB_REPO}/releases" 2>&1 | sed -ne 's/^  "id": \(.*\),$/\1/p')
-
-curl -u "${GITHUB_LOGIN_USERNAME}:${GITHUB_ACCESS_TOKEN}" -X POST -H "Content-Type: application/gzip" --data-binary "@${WORKING_DIR}/alpine.tar.gz" "https://uploads.github.com/repos/${GITHUB_REPO}/releases/${GITHUB_RELEASE_ID}/assets?name=${TAG_NAME}.tar.gz"
-
-echo "Done!"
+einfo "Done!"
 
 # tar zxvf alpine.tar.gz && sudo BUILDROOT_OUTPUT_DIR=alpinebuild/ ./chip-fel-flash.sh
